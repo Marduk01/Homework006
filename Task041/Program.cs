9e404c71faf95cdda0c86d0c3562e71889fe6cc8
Igor Bodalov <igorbodalov@gmail.com>
2023-04-03 12:50:37 +0300

    task041

---
+++ Task041/Program.cs
@@ -0,0 +1,19 @@
+﻿// Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+// -1, -7, 567, 89, 223-> 3
+
+Console.Write("Введите числа через пробел : ");
+int k = Convert.ToInt32(Console.ReadLine());
+int[] massiveChisel = new int[k];
+
+void inputNumbers(int k)
+{
+    for (int i = 0; i < k; i++)
+    {
+        Console.Write($"  ");
+        massiveChisel[i] = Convert.ToInt32(Console.ReadLine());
+    }
+}
+
